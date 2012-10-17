995a1d2b80d01f723b348c9a1f05fb94c73373d0
Taylor Bowland <taylor.bowland@gmail.com>
2012-10-16 21:21:23 -0700

    Added link to site

--- craigs_photo/popup.js
+++ craigs_photo/popup.js
@@ -7,7 +7,12 @@
 
 chrome.tabs.getSelected(null, function(tab) {
   var tab_url = tab.url;
-  var html = '<center><br />\n  <img src="' + tab.url + '">\n<br /></center>'
+  var html = '\
+  <center>\n<br />\n\
+    <a href="http://www.esotours.com">\
+     \n<img src="' + tab.url + '">\n</a>\
+    \n<br />\n\
+  </center>'
 
   // Add URL
   document.getElementById('url').innerText = html
