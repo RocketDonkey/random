9dd6bd06c0ab6c6fffb9e90c4484309ba83a66b8
Taylor Bowland <taylor@taylorbowland.com>
2014-12-29 00:38:33 -0800

    A very simple tool to convert from RGB to hex and vice versa.

---
+++ rgbhex/rgbhex.go
@@ -0,0 +1,107 @@
+// Simple command-line tool to convert hex to RGB or vice versa.
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// An rgbval is a slice containing the RGB values passed to the -rgb flag.
+type rgbval RGB
+
+// The flag representing the comma-delimited RGB value.
+var rgbflag rgbval
+
+// The -rgb flag needs to satisfy the flag.Value interface, which requires a
+// String and Set method.
+func (rgbval *rgbval) String() string {
+	return fmt.Sprint(*rgbval)
+}
+
+// Convert a comma-separated string of RGB values into a value of type RGB.
+func (rgbval *rgbval) Set(value string) error {
+	rgb_slice := strings.Split(value, ",")
+	if len(rgb_slice) != 3 {
+		return errors.New(
+			"Invalid value for flag rgb. Flag value should be in r,g,b format")
+	}
+
+	// Set the values of the flag.
+	rgbval.red, _ = strconv.ParseInt(rgb_slice[0], 0, 64)
+	rgbval.green, _ = strconv.ParseInt(rgb_slice[1], 0, 64)
+	rgbval.blue, _ = strconv.ParseInt(rgb_slice[2], 0, 64)
+	return nil
+}
+
+// An RGB contains the (R)ed, (G)reen and (B)lue values for a color.
+type RGB struct {
+	red   int64
+	green int64
+	blue  int64
+}
+
+// Output RGB in a human readable format.
+func (rgb *RGB) String() string {
+	return fmt.Sprintf("RGB{R:%d, G:%d, B:%d}", rgb.red, rgb.green, rgb.blue)
+}
+
+// HexToRGB converts a hex color value to RGB.
+func HexToRGB(hex_val int64) *RGB {
+	// For a hex value of #AABBCC, the RGB values are:
+	//   R: AA
+	//   G: BB
+	//   B: CC
+
+	// Red: Right-shift the value 16 bits.
+	red := hex_val >> 16
+
+	// Green: Right-shift 8 bits and then bitwise AND with 8 bits of 1's.
+	green := (hex_val >> 8) & 0xFF
+
+	// Blue: bitwise AND with 8 bits of 1's.
+	blue := hex_val & 0xFF
+	return &RGB{red, green, blue}
+}
+
+// RGBToHex converts an color value to hex.
+func RGBToHex(rgb_val *rgbval) string {
+	// To convert the RGB value to hex, we can convert each individual value to
+	// hex and then concatenate the results together.
+	// Example (R, G, B): (10, 15 20) -> (A, F, 14) -> 0A0F14
+	var buffer bytes.Buffer
+	buffer.WriteString(fmt.Sprintf("%02x", rgb_val.red))
+	buffer.WriteString(fmt.Sprintf("%02x", rgb_val.green))
+	buffer.WriteString(fmt.Sprintf("%02x", rgb_val.blue))
+	return buffer.String()
+}
+
+func main() {
+	// Parse the command line flags.
+	hexPtr := flag.String("hex", "", "a hex value to convert to RGB")
+	flag.Var(&rgbflag, "rgb", "an RGB value to convert to hex")
+	flag.Parse()
+
+	// If neither flag is provided, print usage and exit.
+	if flag.NFlag() == 0 || flag.NFlag() == 2 {
+		fmt.Println("Usage: rgbhex [-hex=ABCDEF | -rgb=123,234,100]")
+		os.Exit(1)
+	}
+
+	if *hexPtr != "" {
+		// Convert the provided hex value into an integer.
+		hex_val, err := strconv.ParseInt(*hexPtr, 16, 32)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(HexToRGB(hex_val))
+	} else {
+		// Convert an RGB value to hex.
+		fmt.Println(RGBToHex(&rgbflag))
+	}
+}
